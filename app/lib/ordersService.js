d1baaf4d06b14bb89d01821649f146124b64693e
Gabriel Viana <gaa.vianaa@gmail.com>
2025-10-14 10:48:40 -0300

    mudando a forma de exibir

--- app/lib/ordersService.js
+++ app/lib/ordersService.js
@@ -1,25 +1,28 @@
 import { supabase } from "./supabase";
 
 export async function buscarPedidos() {
-    const { data, error } = await supabase
+  const { data, error } = await supabase
     .from("pedidos")
     .select("*")
     .eq("status", "pendente");
 
-    if (error) {
-        console.error("Erro ao buscar pedidos:", error)
-        return []
+  if (error) {
+    console.error("Erro ao buscar pedidos:", error);
+    return [];
+  }
+
+  const grupo = {};
+
+  data.forEach((item) => {
+    if (!grupo[item.pedido_uuid]) {
+      grupo[item.pedido_uuid] = {
+        pedido_uuid: item.pedido_uuid,
+        mesa_id: item.mesa_id,
+        itens: [],
+      };
     }
-    
-    const grupo = data.reduce((acc, item) => {
-        if (!acc[item.mesa_id]) {
-            acc[item.mesa_id] = []      
-        }
-        acc[item.mesa_id].push(item)
-        return acc;
-    }, {});
-    return Object.keys(grupo).map((mesaId) => ({
-        mesa_id: mesaId,
-        itens: grupo[mesaId]
-    }))
+    grupo[item.pedido_uuid].itens.push(item);
+  });
+
+  return Object.values(grupo);
 }
